b859ebb3230292ebd37ee5228d1ca1204add108a
Worachat <worachat.smn@gmail.com>
2025-03-08 17:22:07 +0700

    delete: run test

--- test_code/test_FizzBuzz.py
+++ test_code/test_FizzBuzz.py
@@ -28,6 +28,3 @@ class TestFizzBuzz(unittest.TestCase):
         expected = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", 
                     "11", "Fizz", "13", "14", "FizzBuzz"]
         self.assertEqual(result, expected)
-
-if __name__ == "__main__":
-    unittest.main()
